38bb701c3af1b32467234eabbc4e3c6c52731396
Pramod Mannapperuma <96408353+PramodMannapperuma@users.noreply.github.com>
2023-08-21 21:31:28 +0530

    Added linked in learning files

---
+++ Linkedin_Lerning/LinkedIn_Learnig_Java/src/StudentProfile.java
@@ -0,0 +1,19 @@
+//public class StudentProfile {
+//    String firstName;
+//    String lastName;
+//    String declaredMajor;
+//    double gpa;
+//    int YearToGraduate;
+//    public StudentProfile(String firstName, String lastName, String declaredMajor, double gpa, int YearToGraduate){
+//        this.firstName = firstName;
+//        this.lastName = lastName;
+//        this.declaredMajor = declaredMajor;
+//        this.gpa = gpa;
+//        this.YearToGraduate = YearToGraduate;
+//
+//    }
+//
+//    public void GraduationYear() {
+//        this.YearToGraduate = this.YearToGraduate + 1;
+//    }
+//}
---
+++ Linkedin_Lerning/LinkedIn_Learnig_Java/src/Triangle.java
@@ -0,0 +1,23 @@
+//public class Triangle {
+//    double base;
+//    double height;
+//    double sideline;
+//    double sideLineTwo;
+//    double sideLineThree;
+//
+//    public Triangle(double base,
+//    double height,
+//    double sideline,
+//    double sideLineTwo,
+//    double sideLineThree) {
+//        this.base = base;
+//        this.height = height;
+//        this.sideline = sideline;
+//        this.sideLineTwo = sideLineTwo;
+//        this.sideLineThree = sideLineThree;
+//    }
+//
+//    public double findArea() {
+//        return (this.base * this.height) / 2;
+//    }
+//}
